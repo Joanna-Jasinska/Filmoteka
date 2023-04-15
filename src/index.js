62f2564540e82c8bed420bb6047ef7d34991ced2
Wiecek-K <krystiandwiecek@gmail.com>
2023-04-16 00:28:16 +0200

    added loader when we fetch data

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import './sass/main.scss';
-import { fetchPopular } from './js/fetch';
-import { showLoader, removeLoader, initLoader } from './js/loader';
+import "./sass/main.scss";
+import { fetchPopular } from "./js/fetch";
+import { removeLoader, initLoader } from "./js/loader";
 
 initLoader();
 
 //fech danych z serwera każdorazowo odpala>>>
-showLoader();
+// showLoader();
 
 // spełnienie promise/pobranie danych  z serwera(tutaj zastąpione timeoutem)>>>
-setTimeout(() => {
-  removeLoader();
-}, 2000);
+// setTimeout(() => {
+//   removeLoader();
+// }, 2000);
 
 fetchPopular()
-  .then(popularMovies => console.log(popularMovies))
-  .catch(error => console.error(error));
+	.then((popularMovies) => console.log(popularMovies))
+	.catch((error) => console.error(error))
+	.finally(() => {
+		removeLoader();
+	});
