e58a8f21989cae5ff89e67d547f328330ea1a41e
Magdalena Kuśmierczak <magdalena-jarek@o2.pl>
2023-04-16 14:48:53 +0200

    header anf footer basics

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import './sass/main.scss';
-import { fetchPopular } from './js/fetch';
-import { showLoader, removeLoader, initLoader } from './js/loader';
+// import { fetchPopular } from './js/fetch';
+// import { showLoader, removeLoader, initLoader } from './js/loader';
 
-initLoader();
+// initLoader();
 
-//fech danych z serwera każdorazowo odpala>>>
-showLoader();
+// //fech danych z serwera każdorazowo odpala>>>
+// showLoader();
 
-// spełnienie promise/pobranie danych  z serwera(tutaj zastąpione timeoutem)>>>
-setTimeout(() => {
-  removeLoader();
-}, 2000);
+// // spełnienie promise/pobranie danych  z serwera(tutaj zastąpione timeoutem)>>>
+// setTimeout(() => {
+//   removeLoader();
+// }, 2000);
 
-fetchPopular()
-  .then(popularMovies => console.log(popularMovies))
-  .catch(error => console.error(error));
+// fetchPopular()
+//   .then(popularMovies => console.log(popularMovies))
+//   .catch(error => console.error(error));
