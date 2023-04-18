79ea2e6e0f5ed5edabb475a59d7a26bca9d0302f
Dominika-Kuros <113472378+Dominika-Kuros@users.noreply.github.com>
2023-04-18 18:28:28 +0200

    fetch and render HTML

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,38 +1,65 @@
 const refs = {
-	modal: document.querySelector("[data-modal]"),
-	openModal: document.querySelector("[data-modal-open]"),
-	closeModal: document.querySelector("[data-modal-close]"),
-	backdrop: document.querySelector(".backdrop"),
+  modal: document.querySelector('[data-modal]'),
+  openModal: document.querySelector('[data-modal-open]'),
+  closeModal: document.querySelector('[data-modal-close]'),
+  backdrop: document.querySelector('.backdrop'),
 };
 
-refs.closeModal.addEventListener("click", toggleModal);
+refs.closeModal.addEventListener('click', toggleModal);
 
 export function toggleModal() {
-	refs.backdrop.classList.toggle("is-hidden");
+  refs.backdrop.classList.toggle('is-hidden');
 }
 
-export const fetchMovieId = async (movie_id) => {
-	try {
-		const response = await fetch(
-			`https://api.themoviedb.org/3/movie/${movie_id}?api_key=964358699754c21d74c014b561cf196c&language=en-US`,
-		);
-		const movie = await response.json();
-		return movie;
-	} catch (error) {
-		console.log(error);
-	}
+export const fetchMovieId = async movie_id => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movie_id}?api_key=964358699754c21d74c014b561cf196c&language=en-US`,
+    );
+    const movie = await response.json();
+    return movie;
+  } catch (error) {
+    console.log(error);
+  }
 };
-export const renderModal = (movie) => {
-	const modalMarkup = `<div class="movie-details">
-    <img src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}">
-    <h2>${movie.title}</h2>
-    <p>Vote / Votes ${movie.vote_average} / ${movie.vote_count}</p>
-    <p>Popularity ${movie.popularity}</p>
-    <p>Original Title ${movie.original_title}</p>
-    <p>Genre ${movie.genres}</p>
-    <p>ABOUT ${movie.overview}</p>
-    <button class="watched"> </button>
-    <button class="queue"> </button>
-    </div>`;
-	refs.modal.innerHTML = modalMarkup;
+export const renderModal = movie => {
+  const modalMarkup = `<div class="movie-details">
+  <img
+    class="movie-details__image"
+    src="https://image.tmdb.org/t/p/w500${movie.backdrop_path}"
+  />
+  <h2 class="movie-details__title">${movie.title}</h2>
+  <ul class="movie-details__list">
+    <li class="movie-details__list-element">
+      <p class="movie-details__list-pararaph">
+        Vote / Votes
+        <span class="movie-details__average">${movie.vote_average}</span> /
+        <span class="movie-details__vote-count">${movie.vote_count}</span>
+      </p>
+    </li>
+    <li class="movie-details__list-element">
+      <p class="movie-details__list-pararaph">
+        Popularity <span class="movie-details__popularity">${movie.popularity}</span>
+      </p>
+    </li>
+    <li class="movie-details__list-element">
+      <p class="movie-details__list-pararaph">
+        Original Title
+        <span class="movie-details__original-title">${movie.original_title}</span>
+      </p>
+    </li>
+    <li class="movie-details__list-element">
+      <p class="movie-details__list-pararaph">
+        Genre <span class="movie-details__genres">${movie.genres}</span>
+      </p>
+    </li>
+  </ul>
+  <h3 class="movie-details__about-title">ABOUT</h3>
+  <p class="movie-details__overview">${movie.overview}</p>
+  <div buttons-modal>
+    <button class="buttons-modal__watched">Add to watched</button>
+    <button class="buttons-modal__queue">Add to queue</button>
+  </div>
+</div>`;
+  refs.modal.innerHTML = modalMarkup;
 };
