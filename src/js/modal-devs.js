a04804b8f29080a713dfc7732427fdb884588836
Danka Proszkiewicz <danka.proszk@gmail.com>
2023-04-21 11:55:36 +0200

    update

--- src/js/modal-devs.js
+++ src/js/modal-devs.js
@@ -1,6 +1,6 @@
 const modalDevLink = document.querySelector('.footer-highlight');
 const modalDevWindow = document.querySelector('.modal-devs');
-const btns = document.querySelectorAll("[data-slider-button]");
+const btns = document.querySelectorAll('[data-slider-button]');
 
 export function openModalDevs() {
   modalDevWindow.classList.remove('modal-devs-hidden');
@@ -11,23 +11,27 @@ export function closeModalDevs() {
 }
 
 btns.forEach(button => {
-    button.addEventListener("click", () => {
-        const offset = button.dataset.sliderButton === "next" ? 1: -1;
-        const slides = button.closest(".modal-devs__slider").querySelector(".modal-devs__slide-list");
-        const activeSlide = slides.querySelector('[data-active]');
-        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-        if (newIndex <0) newIndex = slides.children.length -1
-        if (newIndex >= slides.children.length) newIndex = 0;
+  button.addEventListener('click', () => {
+    const offset = button.dataset.sliderButton === 'next' ? 1 : -1;
+    const slides = button.closest('.modal-devs__slider').querySelector('.modal-devs__slide-list');
+    const activeSlide = slides.querySelector('[data-active]');
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
 
-        slides.children[newIndex].dataset.active= true;
-        delete activeSlide.dataset.active
-
-    })
-})
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+  });
+});
 
 modalDevLink.addEventListener('click', openModalDevs);
 window.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-        closeModalDevs();
-    }
-  });
+  if (event.key === 'Escape') {
+    closeModalDevs();
+  }
+});
+window.addEventListener('click', event => {
+  if (event.target.classList.contains('modal-devs')) {
+    closeModalDevs();
+  }
+});
