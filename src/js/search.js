5b560cbe574413bcc6e1ac70379b26540da6302d
Maciej Kociemski <maciej.kociemski@gmail.com>
2023-04-24 23:31:07 +0200

    feat: add debounce
    is working

--- src/js/search.js
+++ src/js/search.js
@@ -59,9 +59,28 @@ export function displayMovies(movies, maxGenres = 2) {
     });
   });
 }
+async function searchWithDebounce(query, delay) {
+  let timeoutId;
 
-async function handleSearch(event) {
-  event.preventDefault();
+  return new Promise((resolve, reject) => {
+    timeoutId = setTimeout(async () => {
+      try {
+        const movies = await searchMovies(query);
+        resolve(movies);
+      } catch (error) {
+        reject(error);
+      }
+    }, delay);
+  })
+    .finally(() => {
+      clearTimeout(timeoutId);
+    });
+}
+
+let searchTimeoutId;
+
+async function handleSearch(e) {
+  e.preventDefault();
   const searchInput = document.querySelector('.search-form--input');
   const query = searchInput.value;
   const headerAlert = document.querySelector('.notification-alert');
@@ -70,15 +89,28 @@ async function handleSearch(event) {
     headerAlert.textContent = 'Search result not successful. Enter the correct movie name';
     return;
   }
-  const movies = await searchMovies(query);
-  if (movies.length === 0) {
-    headerAlert.textContent = 'There is no such movie in the Filmoteka, please search again';
-    return;
-  }
 
   headerAlert.textContent = '';
 
-  displayMovies(movies);
+  showLoader();
+
+  if (searchTimeoutId) {
+    clearTimeout(searchTimeoutId);
+  }
+
+  searchTimeoutId = setTimeout(async () => {
+    try {
+      const movies = await searchWithDebounce(query, 500);
+      if (movies.length === 0) {
+        headerAlert.textContent = 'There is no such movie in the Filmoteka, please search again';
+      }
+      displayMovies(movies);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      removeLoader();
+    }
+  }, 500);
 }
 
 const searchButton = document.querySelector('.search-form--button');
