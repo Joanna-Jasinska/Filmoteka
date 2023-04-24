782b0316154e4530bae3dc2d27a34c67549d4381
Danka Proszkiewicz <danka.proszk@gmail.com>
2023-04-24 06:45:53 +0200

    added keys navigation

--- src/js/modal-devs.js
+++ src/js/modal-devs.js
@@ -1,21 +1,22 @@
-const modalDevLink = document.querySelector('.footer-highlight');
-const modalDevWindow = document.querySelector('.modal-devs');
-const btns = document.querySelectorAll('[data-slider-button]');
+const modalDevsLink = document.querySelector('.footer-highlight');
+const modalDevsWindow = document.querySelector('.modal-devs');
+const modalDevsBtns = document.querySelectorAll('[data-slider-button]');
+const modalDevsImages = [...document.querySelectorAll('.modal-devs__slide')];
 
 export function openModalDevs() {
-  modalDevWindow.classList.remove('modal-devs-hidden');
+  modalDevsWindow.classList.remove('modal-devs-hidden');
 }
 
 export function closeModalDevs() {
-  modalDevWindow.classList.add('modal-devs-hidden');
+  modalDevsWindow.classList.add('modal-devs-hidden');
 }
 
-btns.forEach(button => {
+modalDevsBtns.forEach(button => {
   button.addEventListener('click', () => {
-    const offset = button.dataset.sliderButton === 'next' ? 1 : -1;
+    const btnMoves = button.dataset.sliderButton === 'next' ? 1 : -1;
     const slides = button.closest('.modal-devs__slider').querySelector('.modal-devs__slide-list');
     const activeSlide = slides.querySelector('[data-active]');
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    let newIndex = [...slides.children].indexOf(activeSlide) + btnMoves;
     if (newIndex < 0) newIndex = slides.children.length - 1;
     if (newIndex >= slides.children.length) newIndex = 0;
 
@@ -24,7 +25,35 @@ btns.forEach(button => {
   });
 });
 
-modalDevLink.addEventListener('click', openModalDevs);
+modalDevsBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    const btnMoves = button.dataset.sliderButton === 'next' ? 1 : -1;
+    navImages(btnMoves);
+  });
+});
+
+export function navImages(btnMoves) {
+  const activeImage = document.querySelector('.modal-devs__slide[data-active]');
+  if (!activeImage) {
+    return;
+  }
+  let newIndex = modalDevsImages.indexOf(activeImage) + btnMoves;
+  if (newIndex < 0) newIndex = modalDevsImages.length - 1;
+  if (newIndex >= modalDevsImages.length) newIndex = 0;
+
+  activeImage.removeAttribute('data-active');
+  modalDevsImages[newIndex].setAttribute('data-active', '');
+}
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'ArrowLeft') {
+    navImages(-1);
+  } else if (event.key === 'ArrowRight') {
+    navImages(1);
+  }
+});
+
+modalDevsLink.addEventListener('click', openModalDevs);
 window.addEventListener('keydown', event => {
   if (event.key === 'Escape') {
     closeModalDevs();
