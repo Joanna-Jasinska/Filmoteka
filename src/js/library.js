b76c4acbce8eb5e143fdade9b01de3fee0c3acbb
Maciej1999 <maciej.sikora.1999@wp.pl>
2023-04-25 18:18:19 +0200

    Działające usuwanie

--- src/js/library.js
+++ src/js/library.js
@@ -11,9 +11,9 @@ import { renderModal, showModal, fetchMovieById } from './modal.js';
 const queueIds = JSON.parse(localStorage.getItem('queue'));
 const watchedIds = JSON.parse(localStorage.getItem('watched'));
 
-const queueButton = document.querySelector('#queue-btn');
-const watchedButton = document.querySelector('#watched-btn');
-
+const queueButton = document.querySelector('#queue-button');
+const watchedButton = document.querySelector('#watched-button');
+watchedButton.classList.add('is-active');
 const API_KEY = '3453ae595a5d53cbc877c6d05de8a002';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -32,9 +32,13 @@ export async function showWatched() {
   removeLoader();
 }
 queueButton.addEventListener('click', () => {
+  queueButton.classList.add('is-active');
+  watchedButton.classList.remove('is-active');
   showQueue();
 });
 watchedButton.addEventListener('click', () => {
+  queueButton.classList.remove('is-active');
+  watchedButton.classList.add('is-active');
   showWatched();
 });
 
