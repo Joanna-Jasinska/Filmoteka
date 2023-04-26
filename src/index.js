b11e2bee8f72abed97c4afc911b819277b3a86c6
Wiecek-K <krystiandwiecek@gmail.com>
2023-04-26 17:45:01 +0200

    formater Rome>Prettier

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
-import "./sass/main.scss";
-import debounce from "lodash/debounce";
-import { fetchPopular } from "./js/fetch";
-import { removeLoader } from "./js/loader";
-import { displayMovies } from "./js/search";
+import './sass/main.scss';
+import debounce from 'lodash/debounce';
+import { fetchPopular } from './js/fetch';
+import { removeLoader } from './js/loader';
+import { displayMovies } from './js/search';
 
-import "./js/pagination";
-import "./js/modal-devs";
+import './js/pagination';
+import './js/modal-devs';
 
-import {
-	createPagination,
-	fixPaginationBtnsOnWindowChange,
-} from "./js/pagination";
+import { createPagination, fixPaginationBtnsOnWindowChange } from './js/pagination';
 
 (async () => {
-	try {
-		const popularMovies = await fetchPopular();
+  try {
+    const popularMovies = await fetchPopular();
 
-		const movies = popularMovies.results;
-		displayMovies(movies);
-		createPagination(popularMovies, "popular");
-		removeLoader();
-	} catch (error) {
-		console.error(error);
-		removeLoader();
-	}
-	window.addEventListener(
-		"resize",
-		debounce(fixPaginationBtnsOnWindowChange, 30),
-	);
+    const movies = popularMovies.results;
+    displayMovies(movies);
+    createPagination(popularMovies, 'popular');
+    removeLoader();
+  } catch (error) {
+    console.error(error);
+    removeLoader();
+  }
+  window.addEventListener('resize', debounce(fixPaginationBtnsOnWindowChange, 30));
 })();
