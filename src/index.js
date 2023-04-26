d8b33907936019ae1e96a9472da5616e25f9fb72
Wiecek-K <krystiandwiecek@gmail.com>
2023-04-26 17:32:52 +0200

    changed then/catch to async/await

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-import './sass/main.scss';
-import debounce from 'lodash/debounce';
-import { fetchPopular } from './js/fetch';
-import { removeLoader } from './js/loader';
-import { displayMovies } from './js/search';
+import "./sass/main.scss";
+import debounce from "lodash/debounce";
+import { fetchPopular } from "./js/fetch";
+import { removeLoader } from "./js/loader";
+import { displayMovies } from "./js/search";
 
-import './js/pagination';
-import './js/modal-devs';
-// import { removeModal } from './js/modal';
-import { createPagination, fixPaginationBtnsOnWindowChange } from './js/pagination';
+import "./js/pagination";
+import "./js/modal-devs";
 
-fetchPopular()
-  .then(popularMovies => {
-    const movies = popularMovies.results;
-    displayMovies(movies);
-    createPagination(popularMovies, 'popular');
-    // removeModal();
-  })
-  .catch(error => console.error(error))
-  .finally(() => {
-    removeLoader();
-  });
-window.addEventListener('resize', debounce(fixPaginationBtnsOnWindowChange, 30));
+import {
+	createPagination,
+	fixPaginationBtnsOnWindowChange,
+} from "./js/pagination";
+
+(async () => {
+	try {
+		const popularMovies = await fetchPopular();
+
+		const movies = popularMovies.results;
+		displayMovies(movies);
+		createPagination(popularMovies, "popular");
+		removeLoader();
+	} catch (error) {
+		console.error(error);
+		removeLoader();
+	}
+	window.addEventListener(
+		"resize",
+		debounce(fixPaginationBtnsOnWindowChange, 30),
+	);
+})();
